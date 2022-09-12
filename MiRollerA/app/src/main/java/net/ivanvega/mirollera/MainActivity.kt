a2d34fadfd940b687bc5e68a8524e8862ac1eb5c
MarioJimenez9 <luismrj999@gmail.com>
2022-09-12 00:03:45 -0500

    Practica Linear Layout and Resources

--- MiRollerA/app/src/main/java/net/ivanvega/mirollera/MainActivity.kt
+++ MiRollerA/app/src/main/java/net/ivanvega/mirollera/MainActivity.kt
@@ -24,57 +24,39 @@ class MainActivity : AppCompatActivity() {
         img = findViewById(R.id.imgRoller)
         imq2 = findViewById(R.id.imgRoller2)
 
-        /*boton.setOnClickListener(
-            View.OnClickListener {
-                Toast.makeText(applicationContext,
-                    "Boton presionafdo",
-                    Toast.LENGTH_LONG
-                    ).show()
-            }
-        )*/
-
         img.setImageDrawable(resources.getDrawable(R.drawable.dice_6))
         imq2.setImageDrawable(resources.getDrawable(R.drawable.dice_6))
 
         boton.setOnClickListener {
-            val al = getRandomDiceImage()
-            val al2 = getRandomDiceImage()
+            val id1 = diceRoller()
+            val id2 = diceRoller()
+            val d1 = getRandomDiceImage(id1)
+            val d2 = getRandomDiceImage(id2)
             Toast.makeText(
                 applicationContext,
-                "Boton presionado aleatorio\n Dado 1: " + al
-                + "\nDado 2: " + al2,
+                "Boton presionado aleatorio\n Dado 1: " + id1
+                + "\nDado 2: " + id2,
                 Toast.LENGTH_LONG
             ).show()
-
-
-
-            val idImagenAl = when (al){
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                6 -> R.drawable.dice_6
-                else -> R.drawable.empty_dice
-            }
-            val idImagenAl2 = when (al2){
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                6 -> R.drawable.dice_6
-                else -> R.drawable.empty_dice
-            }
-
-            img.setImageResource(idImagenAl)
-            imq2.setImageResource(idImagenAl2)
-            lbl.text = (al + al2).toString()
+            img.setImageResource(d1)
+            imq2.setImageResource(d2)
+            lbl.text = (id1 + id2).toString()
 
         }
     }
-    private fun getRandomDiceImage() : Int {
-        return(1..6).random()
+    private fun getRandomDiceImage(al: Int) : Int {
+        val idImagenAl = when (al){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        return idImagenAl;
     }
-
+    private fun diceRoller() : Int{
+        return (1..6).random()
+    }
 }
